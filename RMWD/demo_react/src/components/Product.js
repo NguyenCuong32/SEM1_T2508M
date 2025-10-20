2f7384de01562123439bccd2ec378001c48804a1
NguyenCuong32 <cuongnx32@fpt.edu.vn>
2025-10-20 09:18:34 +0700

    demo reactjs

---
+++ RMWD/demo_react/src/components/Product.js
@@ -0,0 +1,15 @@
+import './Product.css';
+function Product(){
+    return (
+        <div>
+            <h1>iPhone 17 Pro</h1>
+            <p>Price: $1199</p>
+            <p>Description: The latest iPhone with advanced features and improved performance.</p>
+            <div>
+                <button id ="buyNow">Buy Now</button>
+                <button id = "addToCart">Add to Cart</button>
+            </div>
+        </div>
+    )
+}
+export default Product;
