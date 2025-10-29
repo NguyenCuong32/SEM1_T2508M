f39432c5eb500a646df6dcaa382d9a35d948bc64
NguyenCuong32 <cuongnx32@fpt.edu.vn>
2025-10-29 09:39:52 +0700

    fixed bug router

--- RMWD/demo_react_02/product_managerment/src/App.js
+++ RMWD/demo_react_02/product_managerment/src/App.js
@@ -1,13 +1,11 @@
-
-import { Route, Router, Routes } from 'react-router-dom';
 import './App.css';
 // import ProductPage from './pages/ProductPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Home from './pages/HomePage';
+import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 import AboutMe from './pages/AboutMe';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
@@ -15,7 +13,7 @@ function App() {
         <Header></Header>
         <main className="content">
           <Routes>
-            <Route path="/home" element={<Home />}/>
+            <Route path="/home" element={<HomePage />}/>
             <Route path="/product" element={<ProductPage />} />
             <Route path="/aboutme" element={<AboutMe />} />
           </Routes>
